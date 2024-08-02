507ce9c929eaf112e3d1be55c541de2bc966ca02
AliceAuto <sgq2742798794@gmail.com>
2024-08-02 17:15:41 +0800

    d

---
+++ 工程代码/Games/ShadowWorld/SourceCode/VCProject2012/VCProject2012/include/Headers/NonInteractiveBlock.h
@@ -0,0 +1,17 @@
+#ifndef NONINTERACTIVEBLOCK_H
+#define NONINTERACTIVEBLOCK_H
+#include"StaticEntity.h"
+/*
+ * @brief 这是一个 NonInteractiveBlock类
+ * 继承自 StaticEntity，不可以交互的静态实体类都是一个 NonInteractiveBlock 类
+ *        
+ * @author	孙国庆
+ * @date 2024-07-29
+ */
+class NonInteractiveBlock:public StaticEntity{
+public:
+	NonInteractiveBlock();
+	~NonInteractiveBlock();
+
+};
+#endif
