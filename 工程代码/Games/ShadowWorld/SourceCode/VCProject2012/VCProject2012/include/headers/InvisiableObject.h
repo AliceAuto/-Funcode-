83cad2e33317ce927a0dbc03ecb3fe381f4bb6ae
AliceAuto <sgq2742798794@gmail.com>
2024-08-02 20:13:30 +0800

    新模板

---
+++ 工程代码/Games/ShadowWorld/SourceCode/VCProject2012/VCProject2012/include/headers/InvisiableObject.h
@@ -0,0 +1,20 @@
+#ifndef INVISIABLEOBJECT_H
+#define INVISIABLEOBJECT_H
+#include"Object.h"
+
+/*
+ * @brief 这是一个 InvisibleObject类
+ * 继承自 Object
+ *        
+ * @author	孙国庆
+ * @date 2024-07-29
+ */
+class InvisibleObject :public Object
+{
+public:
+
+	InvisibleObject(){};
+	~InvisibleObject(){};
+};
+
+#endif
