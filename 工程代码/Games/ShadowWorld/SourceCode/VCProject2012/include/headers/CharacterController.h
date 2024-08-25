6be6ae5bbfa38a62e01de6f46c2c60e04440ac48
AliceAuto <sgq2742798794@gmail.com>
2024-08-25 16:53:59 +0800

    Revert "凤"
    This reverts commit 1d43e2da838ace62f4d005d8e5558b1f6b4fecd1.

---
+++ 工程代码/Games/ShadowWorld/SourceCode/VCProject2012/include/headers/CharacterController.h
@@ -0,0 +1,31 @@
+#ifndef CHARACTERCONTROLLER_H
+#define CHARACTERCONTROLLER_H
+
+#include "Entity.h"
+
+class CharacterController : public Entity {
+public:
+<<<<<<< Updated upstream
+    CharacterController(float initialX, float initialY,  ResourceBag* resourceBagPtr);
+    virtual ~CharacterController(); // 虚析构函数
+
+=======
+    CharacterController(float initialX, float initialY,std::string & resourceBag);
+    virtual ~CharacterController(); // 虚析构函数
+	void Init () override;
+>>>>>>> Stashed changes
+    // 处理输入（虚函数）
+    virtual void ProcessInput(const Event& event);
+
+protected:
+    // 提供默认实现，允许子类选择性重写
+    virtual void UpdateState() override;
+    virtual void UpdateAnimation() override;
+    virtual void UpdateSound() override;
+
+    // 角色特有属性
+    enum Facings { Up, Down, Left, Right };
+    Facings facing;
+};
+
+#endif // CHARACTERCONTROLLER_H
