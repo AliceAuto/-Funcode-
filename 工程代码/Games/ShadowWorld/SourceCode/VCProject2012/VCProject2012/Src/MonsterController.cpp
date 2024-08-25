f2f6aed73e77a29d78b80d82fa63bd45f5378bf4
AliceAuto <sgq2742798794@gmail.com>
2024-08-25 15:44:07 +0800

    Reapply "Reapply "f""
    This reverts commit 70f0e0d588ed5d9f629a037a118ba6a4cfda2a36.

--- 工程代码/Games/ShadowWorld/SourceCode/VCProject2012/VCProject2012/Src/MonsterController.cpp
+++ 工程代码/Games/ShadowWorld/SourceCode/VCProject2012/VCProject2012/Src/MonsterController.cpp
@@ -22,8 +22,13 @@
 
 
 // 构造函数
+<<<<<<< Updated upstream
 MonsterController::MonsterController(float initialX, float initialY, ResourceBag * resourceBagPtr)
     : CharacterController(initialX,initialY,resourceBagPtr) {}
+=======
+MonsterController::MonsterController(float initialX, float initialY,std::string & resourceBag)
+    : CharacterController(initialX,initialY,resourceBag) {}
+>>>>>>> Stashed changes
 
 // 析构函数
 MonsterController::~MonsterController() {}
